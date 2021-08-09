7f3c40ee7ecc78f65a3efab187d239b833dcdce6
EstherTMY <mtang724@gmail.com>
2021-08-09 12:51:18 -0400

    add eval

--- unsupervised_Cora_Citeseer/execute.py
+++ unsupervised_Cora_Citeseer/execute.py
@@ -9,12 +9,136 @@ import pdb
 import aug
 import os
 import argparse
+import statistics
 
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+# MLP with linear outputs (without softmax)
+class MLP(nn.Module):
+    def __init__(self, num_layers, input_dim, hidden_dim, output_dim):
+        '''
+            num_layers: number of layers in the neural networks (EXCLUDING the input layer). If num_layers=1, this reduces to linear model.
+            input_dim: dimensionality of input features
+            hidden_dim: dimensionality of hidden units at ALL layers
+            output_dim: number of classes for prediction
+            device: which device to use
+        '''
+
+        super(MLP, self).__init__()
+
+        self.linear_or_not = True  # default is linear model
+        self.num_layers = num_layers
+
+        if num_layers < 1:
+            raise ValueError("number of layers should be positive!")
+        elif num_layers == 1:
+            # Linear model
+            self.linear = nn.Linear(input_dim, output_dim)
+        else:
+            # Multi-layer model
+            self.linear_or_not = False
+            self.linears = torch.nn.ModuleList()
+            self.batch_norms = torch.nn.ModuleList()
+
+            self.linears.append(nn.Linear(input_dim, hidden_dim))
+            for layer in range(num_layers - 2):
+                self.linears.append(nn.Linear(hidden_dim, hidden_dim))
+            self.linears.append(nn.Linear(hidden_dim, output_dim))
+
+            for layer in range(num_layers - 1):
+                self.batch_norms.append(nn.BatchNorm1d((hidden_dim)))
+
+    def forward(self, x):
+        if self.linear_or_not:
+            # If linear model
+            return self.linear(x)
+        else:
+            # If MLP
+            h = x
+            for layer in range(self.num_layers - 1):
+                h = F.relu(self.batch_norms[layer](self.linears[layer](h)))
+            return self.linears[self.num_layers - 1](h)
+
+
+
+class NodeClassificationDataset(Dataset):
+    def __init__(self, node_embeddings, labels):
+        self.len = node_embeddings.shape[0]
+        self.x_data = node_embeddings
+        self.y_data = labels
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.len
+
+def train_real_datasets(emb, node_labels, nb_classes):
+    device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
+    print(emb.shape)
+    class_number = nb_classes
+    input_dims = emb.shape
+    FNN = MLP(num_layers=4, input_dim=input_dims[1], hidden_dim=input_dims[1] // 2, output_dim=class_number).to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(FNN.parameters())
+    dataset = NodeClassificationDataset(emb, node_labels)
+    split = process.DataSplit(dataset, shuffle=True)
+    train_loader, val_loader, test_loader = split.get_split(batch_size=64, num_workers=0)
+    print(len(train_loader.dataset))
+    print(len(node_labels), emb.shape)
+    # train_loader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
+    best = float('inf')
+    for epoch in range(100):
+        for i, data in enumerate(train_loader, 0):
+            # data = data.to(device)
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            y_pred = FNN(inputs)
+            loss = criterion(y_pred, labels)
+            print(epoch, i, loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            with torch.no_grad():
+                correct = 0
+                total = 0
+                for data in val_loader:
+                    inputs, labels = data
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+                    outputs = FNN(inputs)
+                    _, predicted = torch.max(outputs.data, 1)
+                    loss = criterion(outputs, labels)
+                    total += labels.size(0)
+                    correct += torch.sum(predicted == labels)
+            if loss < best:
+                best = loss
+                torch.save(FNN.state_dict(), 'best_mlp.pkl')
+            # print(str(epoch), correct / total)
+
+    with torch.no_grad():
+        FNN.load_state_dict(torch.load('best_mlp.pkl'))
+        correct = 0
+        total = 0
+        for data in test_loader:
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = FNN(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += torch.sum(predicted == labels)
+    # print((correct / total).item())
+    return torch.true_divide(correct, total).item()
 
 parser = argparse.ArgumentParser("My DGI")
 
-parser.add_argument('--dataset',          type=str,           default="",                help='data')
-parser.add_argument('--aug_type',         type=str,           default="",                help='aug type: mask or edge')
+parser.add_argument('--dataset',          type=str,           default="texas",                help='data')
+parser.add_argument('--aug_type',         type=str,           default="subgraph",                help='aug type: mask or edge')
 parser.add_argument('--drop_percent',     type=float,         default=0.1,               help='drop percent')
 parser.add_argument('--seed',             type=int,           default=39,                help='seed')
 parser.add_argument('--gpu',              type=int,           default=0,                 help='gpu')
@@ -51,12 +175,15 @@ sparse = True
 
 
 nonlinearity = 'prelu' # special name to separate parameters
-adj, features, labels, idx_train, idx_val, idx_test = process.load_data(dataset)
+# adj, features, labels, idx_train, idx_val, idx_test = process.load_data(dataset)
+adj, features, labels = process.read_real_datasets(dataset)
+print(labels)
 features, _ = process.preprocess_features(features)
 
 nb_nodes = features.shape[0]  # node number
 ft_size = features.shape[1]   # node features dim
-nb_classes = labels.shape[1]  # classes = 6
+nb_classes = int(max(labels)) + 1  # classes = 6
+print(nb_classes)
 
 features = torch.FloatTensor(features[np.newaxis])
 
@@ -132,10 +259,10 @@ if not sparse:
     aug_adj2 = torch.FloatTensor(aug_adj2[np.newaxis])
 
 
-labels = torch.FloatTensor(labels[np.newaxis])
-idx_train = torch.LongTensor(idx_train)
-idx_val = torch.LongTensor(idx_val)
-idx_test = torch.LongTensor(idx_test)
+# labels = torch.FloatTensor(labels[np.newaxis])
+# idx_train = torch.LongTensor(idx_train)
+# idx_val = torch.LongTensor(idx_val)
+# idx_test = torch.LongTensor(idx_test)
 
 model = DGI(ft_size, hid_units, nonlinearity)
 optimiser = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=l2_coef)
@@ -156,9 +283,9 @@ if torch.cuda.is_available():
         aug_adj2 = aug_adj2.cuda()
 
     labels = labels.cuda()
-    idx_train = idx_train.cuda()
-    idx_val = idx_val.cuda()
-    idx_test = idx_test.cuda()
+    # idx_train = idx_train.cuda()
+    # idx_val = idx_val.cuda()
+    # idx_test = idx_test.cuda()
 
 b_xent = nn.BCEWithLogitsLoss()
 xent = nn.CrossEntropyLoss()
@@ -210,49 +337,58 @@ print('Loading {}th epoch'.format(best_t))
 model.load_state_dict(torch.load(args.save_name))
 
 embeds, _ = model.embed(features, sp_adj if sparse else adj, sparse, None)
-train_embs = embeds[0, idx_train]
-val_embs = embeds[0, idx_val]
-test_embs = embeds[0, idx_test]
-
-train_lbls = torch.argmax(labels[0, idx_train], dim=1)
-val_lbls = torch.argmax(labels[0, idx_val], dim=1)
-test_lbls = torch.argmax(labels[0, idx_test], dim=1)
-
-tot = torch.zeros(1)
-tot = tot.cuda()
-
-accs = []
-
-for _ in range(50):
-    log = LogReg(hid_units, nb_classes)
-    opt = torch.optim.Adam(log.parameters(), lr=0.01, weight_decay=0.0)
-    log.cuda()
-
-    pat_steps = 0
-    best_acc = torch.zeros(1)
-    best_acc = best_acc.cuda()
-    for _ in range(100):
-        log.train()
-        opt.zero_grad()
-
-        logits = log(train_embs)
-        loss = xent(logits, train_lbls)
-        
-        loss.backward()
-        opt.step()
-
-    logits = log(test_embs)
-    preds = torch.argmax(logits, dim=1)
-    acc = torch.sum(preds == test_lbls).float() / test_lbls.shape[0]
-    accs.append(acc * 100)
-    print('acc:[{:.4f}]'.format(acc))
-    tot += acc
-
-print('-' * 100)
-print('Average accuracy:[{:.4f}]'.format(tot.item() / 50))
-accs = torch.stack(accs)
-print('Mean:[{:.4f}]'.format(accs.mean().item()))
-print('Std :[{:.4f}]'.format(accs.std().item()))
-print('-' * 100)
+embeddings = torch.squeeze(embeds.cpu().detach())
+acc = []
+for i in range(5):
+    result = train_real_datasets(embeddings, labels, nb_classes)
+    acc.append(result)
+print("mean:")
+print(statistics.mean(acc))
+print("std:")
+print(statistics.stdev(acc))
+# train_embs = embeds[0, idx_train]
+# val_embs = embeds[0, idx_val]
+# test_embs = embeds[0, idx_test]
+#
+# train_lbls = torch.argmax(labels[0, idx_train], dim=1)
+# val_lbls = torch.argmax(labels[0, idx_val], dim=1)
+# test_lbls = torch.argmax(labels[0, idx_test], dim=1)
+#
+# tot = torch.zeros(1)
+# tot = tot.cuda()
+#
+# accs = []
+#
+# for _ in range(50):
+#     log = LogReg(hid_units, nb_classes)
+#     opt = torch.optim.Adam(log.parameters(), lr=0.01, weight_decay=0.0)
+#     log.cuda()
+#
+#     pat_steps = 0
+#     best_acc = torch.zeros(1)
+#     best_acc = best_acc.cuda()
+#     for _ in range(100):
+#         log.train()
+#         opt.zero_grad()
+#
+#         logits = log(train_embs)
+#         loss = xent(logits, train_lbls)
+#
+#         loss.backward()
+#         opt.step()
+#
+#     logits = log(test_embs)
+#     preds = torch.argmax(logits, dim=1)
+#     acc = torch.sum(preds == test_lbls).float() / test_lbls.shape[0]
+#     accs.append(acc * 100)
+#     print('acc:[{:.4f}]'.format(acc))
+#     tot += acc
+#
+# print('-' * 100)
+# print('Average accuracy:[{:.4f}]'.format(tot.item() / 50))
+# accs = torch.stack(accs)
+# print('Mean:[{:.4f}]'.format(accs.mean().item()))
+# print('Std :[{:.4f}]'.format(accs.std().item()))
+# print('-' * 100)
 
 
--- unsupervised_Cora_Citeseer/utils/process.py
+++ unsupervised_Cora_Citeseer/utils/process.py
@@ -6,6 +6,13 @@ from scipy.sparse.linalg.eigen.arpack import eigsh
 import sys
 import torch
 import torch.nn as nn
+import logging
+from functools import lru_cache
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
 
 def parse_skipgram(fname):
     with open(fname) as f:
@@ -209,3 +216,88 @@ def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     values = torch.from_numpy(sparse_mx.data)
     shape = torch.Size(sparse_mx.shape)
     return torch.sparse.FloatTensor(indices, values, shape)
+
+class DataSplit:
+
+    def __init__(self, dataset, test_train_split=0.8, val_train_split=0.2, shuffle=True):
+        self.dataset = dataset
+
+        dataset_size = len(dataset)
+        self.indices = list(range(dataset_size))
+        test_split = int(np.floor(test_train_split * dataset_size))
+
+        if shuffle:
+            np.random.shuffle(self.indices)
+
+        train_indices, self.test_indices = self.indices[:test_split], self.indices[test_split:]
+        train_size = len(train_indices)
+        validation_split = int(np.floor((1 - val_train_split) * train_size))
+
+        self.train_indices, self.val_indices = train_indices[ : validation_split], train_indices[validation_split:]
+
+        self.train_sampler = SubsetRandomSampler(self.train_indices)
+        self.val_sampler = SubsetRandomSampler(self.val_indices)
+        self.test_sampler = SubsetRandomSampler(self.test_indices)
+
+    def get_train_split_point(self):
+        return len(self.train_sampler) + len(self.val_indices)
+
+    def get_validation_split_point(self):
+        return len(self.train_sampler)
+
+    @lru_cache(maxsize=4)
+    def get_split(self, batch_size=50, num_workers=4):
+        logging.debug('Initializing train-validation-test dataloaders')
+        self.train_loader = self.get_train_loader(batch_size=batch_size, num_workers=num_workers)
+        self.val_loader = self.get_validation_loader(batch_size=batch_size, num_workers=num_workers)
+        self.test_loader = self.get_test_loader(batch_size=batch_size, num_workers=num_workers)
+        return self.train_loader, self.val_loader, self.test_loader
+
+    @lru_cache(maxsize=4)
+    def get_train_loader(self, batch_size=50, num_workers=4):
+        logging.debug('Initializing train dataloader')
+        self.train_loader = torch.utils.data.DataLoader(self.dataset, batch_size=batch_size, sampler=self.train_sampler, shuffle=False, num_workers=num_workers)
+        return self.train_loader
+
+    @lru_cache(maxsize=4)
+    def get_validation_loader(self, batch_size=50, num_workers=4):
+        logging.debug('Initializing validation dataloader')
+        self.val_loader = torch.utils.data.DataLoader(self.dataset, batch_size=batch_size, sampler=self.val_sampler, shuffle=False, num_workers=num_workers)
+        return self.val_loader
+
+    @lru_cache(maxsize=4)
+    def get_test_loader(self, batch_size=50, num_workers=4):
+        logging.debug('Initializing test dataloader')
+        self.test_loader = torch.utils.data.DataLoader(self.dataset, batch_size=batch_size, sampler=self.test_sampler, shuffle=False, num_workers=num_workers)
+        return self.test_loader
+
+def read_real_datasets(datasets):
+    edge_path = "/Users/mtang/Documents/baseline_gae/GraphCL/unsupervised_Cora_Citeseer/new_data/{}/out1_graph_edges.txt".format(datasets)
+    node_feature_path = "/Users/mtang/Documents/baseline_gae/GraphCL/unsupervised_Cora_Citeseer/new_data/{}/out1_node_feature_label.txt".format(datasets)
+    with open(edge_path) as edge_file:
+        edge_file_lines = edge_file.readlines()
+        G = nx.parse_edgelist(edge_file_lines[1:], nodetype=int)
+        # g = dgl.from_networkx(G)
+        A = nx.adjacency_matrix(G)
+    with open(node_feature_path) as node_feature_file:
+        node_lines = node_feature_file.readlines()[1:]
+        feature_list = []
+        labels = []
+        max_len = 0
+        for node_line in node_lines:
+            node_id, feature, label = node_line.split("\t")
+            labels.append(int(label))
+            features = feature.split(",")
+            max_len = max(len(features), max_len)
+            feature_list.append([float(feature) for feature in features])
+        feature_pad_list = []
+        for features in feature_list:
+            features += [0] * (max_len - len(features))
+            feature_pad_list.append(features)
+        feature_array = np.array(feature_pad_list)
+        features = torch.from_numpy(feature_array)
+        features = sp.csr_matrix(features)
+        # g.ndata['attr'] = features.float()
+        labels = np.array(labels)
+        labels = torch.FloatTensor(labels).long()
+    return A, features, labels
